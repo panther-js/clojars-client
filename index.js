e20bec148d88d1bbb82ae82211e6e5b198196fc7
Helio Frota <00hf11@gmail.com>
2017-06-11 17:14:34 -0300

    using new code style

--- index.js
+++ index.js
@@ -4,25 +4,11 @@ const roi = require('roi');
 
 const BASE_URL = 'https://clojars.org/api/';
 
-function user (u) {
-  return roi.get({ endpoint: `${BASE_URL}users/${u}` });
-}
-
-function group (g) {
-  return roi.get({ endpoint: `${BASE_URL}groups/${g}` });
-}
-
-function artifact (a) {
-  return roi.get({ endpoint: `${BASE_URL}artifacts/${a}` });
-}
-
-function groupArtifact (g, a) {
-  return roi.get({ endpoint: `${BASE_URL}artifacts/${g}/${a}` });
-}
-
-function search (term) {
-  return roi.get({ endpoint: `https://clojars.org/search?q=${term}&format=json` });
-}
+const user = (u) => roi.get({ endpoint: `${BASE_URL}users/${u}` });
+const group = (g) => roi.get({ endpoint: `${BASE_URL}groups/${g}` });
+const artifact = (a) => roi.get({ endpoint: `${BASE_URL}artifacts/${a}` });
+const groupArtifact = (g, a) => roi.get({ endpoint: `${BASE_URL}artifacts/${g}/${a}` });
+const search = (term) => roi.get({ endpoint: `https://clojars.org/search?q=${term}&format=json` });
 
 module.exports = {
   user,
